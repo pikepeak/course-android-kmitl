962f08a582192c77886a4a24d0dde09390139a0d
pikepeak <khunach@msn.com>
2017-08-23 16:33:06 +0700

    use test device Nexus 4

---
+++ Lab02/MyHomeWork/app/src/main/java/lab/first/myhomework/MainActivity.java
@@ -0,0 +1,13 @@
+package lab.first.myhomework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.homework2);
+    }
+}
